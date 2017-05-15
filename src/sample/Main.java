8aac21afb40f7656bfde8d4b54441c46b29337dd
Vitalii <flipsun@gmail.com>
2017-05-15 13:12:19 +0300

    fix css style

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -17,24 +17,8 @@ public class Main extends Application {
         primaryStage.setResizable(false);
         Scene scene = new Scene(root, 260,390); // width = 260/4=65 high = 390/6=65
         scene.getStylesheets().add("sample/calculator.css");
-        /*root.setStyle("-fx-padding: 10;" +
-                "-fx-border-style: solid inside;" +
-                "-fx-border-width: 2;" +
-                "-fx-border-insets: 5;" +
-                "-fx-border-radius: 5;" +
-                "-fx-border-color: blue;");*/
         primaryStage.setScene(scene);
         primaryStage.show();
-
-
-        scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-            @Override
-            public void handle(KeyEvent event) {
-                switch (event.getCode()) {
-
-                }
-            }
-        });
     }
 
 
